6659d2e67b95066af697de4d32580744079e2882
Victor Ramos <vart96@hotmail.com>
2021-06-11 13:20:51 -0700

    Imported bootstrap 5 from npm

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Container} from 'react-bootstrap-v5'
+
 const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID || ''
 const RESPONSE_TYPE = 'code'
 const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI || 'http://localhost:3000'
@@ -10,9 +12,8 @@ const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=`+
 `&redirect_uri=${REDIRECT_URI}&scope=${encodeURIComponent(SCOPE)}`
 const Login = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+        </Container>
     )
 }
 
